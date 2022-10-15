671b013d9563a5bf96679c47b744323fb852ad3c
ariMrashid <91915317+ariMrashid@users.noreply.github.com>
2022-10-16 00:05:04 +0300

    Update AirlineManager.java

--- Models/AirlineManager.java
+++ Models/AirlineManager.java
@@ -1,1 +1,17 @@
+public class AirlineManager extends Person
+{
+    private String airlineId;
+
+    public AirlineManager(String firstName, String lastName, String email, String username, String password, String Id, String phoneNo, Role role, String airlineId)
+    {
+        super(firstName, lastName, email, username, password, Id, phoneNo, role);
+        this.airlineId = airlineId;
+    }
+
+    public String getAirlineId()
+    {
+        return airlineId;
+    }
+}
+
 
