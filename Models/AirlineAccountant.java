3ab2df5dde452e9a083bf145e55e1b9280a45e7c
ariMrashid <91915317+ariMrashid@users.noreply.github.com>
2022-10-16 00:02:08 +0300

    Update AirlineAccountant.java

--- Models/AirlineAccountant.java
+++ Models/AirlineAccountant.java
@@ -1,1 +1,16 @@
 
+public class AirlineAccountant extends Person
+{
+    private String airlineId;
+
+    public AirlineAccountant(String firstName, String lastName, String email, String username, String password, String Id, String phoneNo, Role role, String airlineId)
+    {
+        super(firstName, lastName, email, username, password, Id, phoneNo, role);
+        this.airlineId = airlineId;
+    }
+
+    public String getAirlineId()
+    {
+        return airlineId;
+    }
+}
