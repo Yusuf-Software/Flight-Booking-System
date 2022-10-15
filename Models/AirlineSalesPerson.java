ebf645151325be50a738f501bab3952b14451d73
Blnd02 <115497808+Blnd02@users.noreply.github.com>
2022-10-16 00:07:29 +0300

    Update AirlineSalesPerson.java

--- Models/AirlineSalesPerson.java
+++ Models/AirlineSalesPerson.java
@@ -1,1 +1,36 @@
+import java.awt.*;
+
+public class AirlineSalesPerson extends Person
+{
+    private String airlineId;
+
+    protected double point;
+    protected ArrayList<Booking> bookingList;
+    protected Image profilePic;
+
+    public AirlineSalesPerson(String firstName, String lastName, String email, String username, String password, String Id, String phoneNo, Role role, String airlineId, double point, ArryaList<Booking> bookingList, Image profilePic)
+    {
+        super(firstName, lastName, email, username, password, Id, phoneNo, role);
+        this.airlineId = airlineId;
+        this.point = point;
+        this.bookingList = bookingList;
+        this.profilePic = profilePic;
+    }
+
+    public double getPoint()
+    {
+        return point;
+    }
+
+    public ArrayList<Booking> getBookingList()
+    {
+        return bookingList;
+    }
+
+    public Image getProfilePic()
+    {
+        return profilePic;
+    }
+
+}
 
