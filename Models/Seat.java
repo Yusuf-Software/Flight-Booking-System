9e6a335a18bd22eda1762732068fe61b7479ea8f
zryann <109387666+zryann@users.noreply.github.com>
2022-10-16 00:05:25 +0300

    Update Seat.java

--- Models/Seat.java
+++ Models/Seat.java
@@ -1,1 +1,16 @@
+public class Seat
+{
+    protected String Id, seatNo;
+    protected ClassName  className;
+
+    public Seat(String Id, String seatNo, ClassName className)
+    {
+        this.Id = Id;
+        this.seatNo = seatNo;
+        this.className = className;
+    }
+
+    enum ClassName{ECONOMY, BUSINESS, FIRST_CLASS}
+}
+
 
