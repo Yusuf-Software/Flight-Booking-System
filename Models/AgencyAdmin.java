b35417130640cd780889f69b2dedff771098a5f0
Blnd02 <115497808+Blnd02@users.noreply.github.com>
2022-10-16 00:05:46 +0300

    Update AgencyAdmin.java

--- Models/AgencyAdmin.java
+++ Models/AgencyAdmin.java
@@ -1,1 +1,14 @@
+public class AgencyAdmin extends Person
+{
+    private String agencyId;
+    public AgencyAdmin(String firstName, String lastName, String email, String username, String password, String Id, String phoneNo, Role role, String agencyId)
+    {
+        super(firstName, lastName, email, username, password, Id, phoneNo, role);
+        this.agencyId = agencyId;
+    }
 
+    public String getAgencyId()
+    {
+        return agencyId;
+    }
+}
