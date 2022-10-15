8ef70f758c25ff00b9bc11db7592e2c95d614e2b
zryann <109387666+zryann@users.noreply.github.com>
2022-10-16 00:03:12 +0300

    Update FlightSeat.java

--- Models/FlightSeat.java
+++ Models/FlightSeat.java
@@ -1,1 +1,11 @@
+public class FlightSeat extends Seat
+{
+    private double price;
+    private Status status;
+    public FlightSeat(String Id, String seatNo, ClassName className)
+    {
+        super(Id, seatNo, className);
+    }
 
+    enum Status{OPEN, BOOKED}
+}
